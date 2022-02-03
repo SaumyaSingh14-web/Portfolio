408419c7377c929bab5110de6281d54fed9838a2
SaumyaSingh14-web <saumyasingh70077@gmail.com>
2022-02-03 17:08:08 +0530

    explore button

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,10 @@
 function Title(props){
     return(
-            <div>
-                <div className={`text-orange-200 p-12 font-bold text-3xl ${props.className}`}>{props.title}</div>
-                <div className={`text-orange-50 text-md text-center mx-3 my-8 ${props.className}`}>{props.description}</div>
-            </div>
-            );
+        <div>
+            <div className={`text-orange-200 p-12 font-bold text-3xl ${props.className}`}>{props.title}</div>
+            <div className={`text-orange-50 text-md text-center mx-3 my-8 ${props.className}`}>{props.description}</div>
+        </div>
+        );
 
 }
 
